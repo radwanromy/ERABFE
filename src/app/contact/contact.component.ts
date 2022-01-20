62bdf3206e4642613c0a858ce53e78a8883be907
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-01-20 17:41:54 +0600

    ERABBE 16

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,8 @@ import { LockerServiceModel } from './home.model';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  // styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   info: any;
